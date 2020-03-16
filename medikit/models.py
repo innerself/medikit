5a639a52ed3a669908c3811445e25b2959dc6577
Sergey Petrin <spetrin@gmail.com>
2020-03-16 22:11:52 +0300

    Add Kit model

--- medikit/models.py
+++ medikit/models.py
@@ -1,8 +1,17 @@
 from django.db import models
 
 
+class Kit(models.Model):
+    name = models.CharField(max_length=256)
+
+
 class Drug(models.Model):
     name = models.CharField(max_length=256)
     description = models.TextField()
     expiration_date = models.DateField()
     added = models.DateTimeField(auto_now_add=True)
+    kit = models.ForeignKey(
+        Kit,
+        on_delete=models.CASCADE,
+        related_name='drugs',
+    )
--- project/settings.py
+++ project/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'medikit.apps.MedikitConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
