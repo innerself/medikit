b104f41a2c0e8c2e959e59f358a2799752111d28
Sergey Petrin <spetrin@gmail.com>
2021-01-26 23:15:05 +0300

    Add STATIC_ROOT setting

--- project/settings.py
+++ project/settings.py
@@ -125,6 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = config('STATIC_ROOT')
 
 LOGIN_REDIRECT_URL = 'medikit:home'
 LOGOUT_REDIRECT_URL = 'medikit:home'
