4e98e5a48869d537c25367e3f78bb811925f586d
Sergey Petrin <spetrin@gmail.com>
2021-02-04 23:17:08 +0300

    Add kit name and user to the admin list view

--- medikit/admin.py
+++ medikit/admin.py
@@ -2,5 +2,17 @@ from django.contrib import admin
 
 from .models import Kit, Medication
 
-admin.site.register(Kit)
-admin.site.register(Medication)
+
+@admin.register(Kit)
+class KitAdmin(admin.ModelAdmin):
+    list_display = ('name', 'user')
+
+
+@admin.register(Medication)
+class MedicationAdmin(admin.ModelAdmin):
+    list_display = ('name', 'kit', 'kit_user')
+
+    def kit_user(self, obj):
+        return obj.kit.user if isinstance(obj.kit, Kit) else "Shared kit"
+
+    kit_user.short_description = 'Kit user'
